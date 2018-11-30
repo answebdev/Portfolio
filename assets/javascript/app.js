f30f3a17b63298a9ecb0e3b2c9d8ff7ee804b30f
ANS <36783010+answebdev@users.noreply.github.com>
2018-11-30 01:44:54 -0800

    added back-to-top button to Portfolio page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -80,3 +80,21 @@ $(document).ready(function () {
 $(document).ready(function () {
 	$('.parallax').parallax();
 });
+
+
+// Back-to-top-button
+var btn = $('#button');
+
+$(window).scroll(function () {
+	if ($(window).scrollTop() > 300) {
+		btn.addClass('show');
+	} else {
+		btn.removeClass('show');
+	}
+});
+
+btn.on('click', function (e) {
+	e.preventDefault();
+	$('html, body').animate({ scrollTop: 0 }, '300');
+});
+
