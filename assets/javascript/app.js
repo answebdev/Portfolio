d4c96a8bc146d9f8bac0d39d39f19a60005ef1d7
ANS <36783010+answebdev@users.noreply.github.com>
2018-12-11 22:46:09 -0800

    added Moving Letters Effect

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,17 +19,45 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // About Me Typewriter Effect
-var i = 0;
-var txt = 'WELCOME TO MY WEBSITE';
-var speed = 50;
+// var i = 0;
+// var txt = 'WELCOME TO MY WEBSITE';
+// var speed = 50;
 
-function typeWriter() {
-	if (i < txt.length) {
-		document.getElementById("about-me").innerHTML += txt.charAt(i);
-		i++;
-		setTimeout(typeWriter, speed);
-	}
-}
+// function typeWriter() {
+// 	if (i < txt.length) {
+// 		document.getElementById("about-me").innerHTML += txt.charAt(i);
+// 		i++;
+// 		setTimeout(typeWriter, speed);
+// 	}
+// }
+
+// About Me Moving Letters Effect - Adapted from: http://tobiasahlin.com/moving-letters/
+// Wrap every letter in a span
+$('.ml2').each(function () {
+	$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({ loop: false })
+	.add({
+		targets: '.ml2 .letter',
+		scale: [4, 1],
+		opacity: [0, 1],
+		translateZ: 0,
+		easing: "easeOutExpo",
+		duration: 950,
+		delay: function (el, i) {
+			return 70 * i;
+		}
+	}).add({
+		targets: '.ml2',
+		opacity: 1,
+		duration: 1000,
+		easing: "easeOutExpo",
+		delay: 1000
+	});
+//    SourceTHURSDAY
+//    SourceSunny mornings
+
 
 // Materialize Sidenav
 $(document).ready(function () {
@@ -110,3 +138,4 @@ btn.on('click', function (e) {
 	e.preventDefault();
 	$('html, body').animate({ scrollTop: 0 }, '300');
 });
+
