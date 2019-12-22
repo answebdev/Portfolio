88c448892e2544329a5f37ff2b848b3b5dcd9937
ANS <36783010+answebdev@users.noreply.github.com>
2019-12-22 09:54:37 -0800

    updated portfolio

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Portfolio
 
-I am a front-end web developer based in San Diego, California, and I enjoy creating clean and dynamic web applications. Although I have a diverse set of skills involving both front-end and back-end technologies, my main passion is front-end development. I love focusing on what the user sees and love learning about new technologies that I can implement for a great user experience. As a user-focused developer, I stand for quality and focus on details in order to produce and develop great web applications and user-friendly experiences.
+I am a front-end web developer based in San Diego, California, and I enjoy creating clean and dynamic web applications. Although I have a diverse set of skills involving both front-end and back-end technologies (see below), my main passion is front-end development. I love focusing on design and what the user sees, and love learning about new technologies that I can implement for a great user experience. As a user-focused front-end developer, I stand for quality and focus on details in order to produce and develop great web applications and user-friendly experiences. Examples of my work and projects can be seen in the portfolio and freelance sections of my website.
 
 Feel free to explore my website and connect with me!
 
-Link to my website:  https://adolfschmuck.com/
+Link to my website: https://adolfschmuck.com/
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,57 @@
 //Landing Page Animation
-$(document).ready(function () {
-	setTimeout(function () {
-		$("#main").removeClass("is-loading");
-	}, 1200)
+$(document).ready(function() {
+  setTimeout(function() {
+    $('#main').removeClass('is-loading');
+  }, 1200);
 });
 
 // About Me Moving Letters Effect - Adapted from: http://tobiasahlin.com/moving-letters/
 // Wrap every letter in a span
-$('.ml2').each(function () {
-	$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-});
+// $('.ml2').each(function () {
+// 	$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+// });
 
-anime.timeline({ loop: false })
-	.add({
-		targets: '.ml2 .letter',
-		scale: [4, 1],
-		opacity: [0, 1],
-		translateZ: 0,
-		easing: "easeOutExpo",
-		duration: 950,
-		delay: function (el, i) {
-			return 70 * i;
-		}
-	}).add({
-		targets: '.ml2',
-		opacity: 1,
-		duration: 1000,
-		easing: "easeOutExpo",
-		delay: 1000
-	});
+// anime.timeline({ loop: false })
+// 	.add({
+// 		targets: '.ml2 .letter',
+// 		scale: [4, 1],
+// 		opacity: [0, 1],
+// 		translateZ: 0,
+// 		easing: "easeOutExpo",
+// 		duration: 950,
+// 		delay: function (el, i) {
+// 			return 70 * i;
+// 		}
+// 	}).add({
+// 		targets: '.ml2',
+// 		opacity: 1,
+// 		duration: 1000,
+// 		easing: "easeOutExpo",
+// 		delay: 1000
+// 	});
 
 // Materialize Sidenav
-$(document).ready(function () {
-	$('.sidenav').sidenav();
+$(document).ready(function() {
+  $('.sidenav').sidenav();
 });
 
 // Materialize Parallax
-$(document).ready(function () {
-	$('.parallax').parallax();
+$(document).ready(function() {
+  $('.parallax').parallax();
 });
 
 // Back-to-top-button
 var btn = $('#button');
 
-$(window).scroll(function () {
-	if ($(window).scrollTop() > 300) {
-		btn.addClass('show');
-	} else {
-		btn.removeClass('show');
-	}
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
 });
 
-btn.on('click', function (e) {
-	e.preventDefault();
-	$('html, body').animate({ scrollTop: 0 }, '300');
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: 0 }, '300');
 });
